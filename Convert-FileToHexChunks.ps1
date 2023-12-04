bcf01667953df6f190e08ae26bdd55e597a3f350
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-12-04 14:23:09 -0600

    Update Convert-FileToHexChunks.ps1

--- Convert-FileToHexChunks.ps1
+++ Convert-FileToHexChunks.ps1
@@ -11,15 +11,27 @@ function Convert-FileToHexChunks {
         # Convert each byte to hexadecimal and join them into a string
         $hexString = ($bytes | ForEach-Object { "{0:X2}" -f $_ }) -join ''
 
-        # Split the hex string into chunks of 16 characters
-        $chunks = [Regex]::Matches($hexString, '.{1,16}')
+        # Create an array to hold the chunks
+        $chunks = @()
+        $index = 0
+
+        # Cut the hex string into random-sized chunks
+        while ($index -lt $hexString.Length) {
+            # Randomly determine the length of the next chunk (between 14 and 18)
+            $chunkLength = Get-Random -Minimum 14 -Maximum 19
+            $chunks += $hexString.Substring($index, [Math]::Min($chunkLength, $hexString.Length - $index))
+            $index += $chunkLength
+        }
 
         # Create a PSObject to store the chunks
         $hexObject = New-Object PSObject
 
+        # Generate a random hex string of 8-16 characters for the first segment
+        $randomHexForFirstSegment = -join ((0..15) | Get-Random -Count (Get-Random -Minimum 8 -Maximum 17) | ForEach-Object { "{0:X}" -f $_ })
+
         # Randomly select a separator for the total number of segments
-        $totalSeparator = Get-Random -InputObject @('G', 'H', 'I')
-        $hexObject | Add-Member -MemberType NoteProperty -Name 1 -Value ($chunks.Count.ToString() + $totalSeparator)
+        $totalSeparator = 'H'
+        $hexObject | Add-Member -MemberType NoteProperty -Name 1 -Value ($chunks.Count.ToString() + $totalSeparator + $randomHexForFirstSegment)
 
         for ($i = 0; $i -lt $chunks.Count; $i++) {
             # Randomly select a separator from G, H, I for each chunk
@@ -28,7 +40,7 @@ function Convert-FileToHexChunks {
             # Add each chunk as a property to the object
             # Prepend each chunk with its segment number and a random separator
             $segmentLabel = ($i + 1).ToString() + $separator
-            $hexObject | Add-Member -MemberType NoteProperty -Name ($i + 2) -Value ($segmentLabel + $chunks[$i].Value)
+            $hexObject | Add-Member -MemberType NoteProperty -Name ($i + 2) -Value ($segmentLabel + $chunks[$i])
         }
 
         # Return the object
