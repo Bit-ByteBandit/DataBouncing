f936e381438d34f4130bbcb5801f3d5c566d0074
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-12-06 04:22:16 -0600

    Create watchLogs.ps1

---
+++ Server/watchLogs.ps1
@@ -0,0 +1,8 @@
+<#
+check fileID list and see if any appear in log 
+keep checking till you find segment 1
+save segement count and filename to variables
+check log for last packet recieved until final packet 
+rebuild 
+remove fileID from list 
+#>
