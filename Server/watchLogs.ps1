94ecd19b0b6df0fd18d9bd9b623769bc5da4b92c
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-12-09 20:05:00 -0600

    Update watchLogs.ps1

--- Server/watchLogs.ps1
+++ Server/watchLogs.ps1
@@ -1,8 +1,38 @@
-<#
-check fileID list and see if any appear in log 
-keep checking till you find segment 1
-save segement count and filename to variables
-check log for last packet recieved until final packet 
-rebuild 
-remove fileID from list 
-#>
+function watchLogs {
+    param (
+        [string]$LogFilePath = "logs.txt",
+        [string]$IdsFilePath = "file-IDs.txt"
+    )
+
+    # Check if the log file exists
+    if (-Not (Test-Path $LogFilePath)) {
+        Write-Host "Log file not found: $LogFilePath"
+        return
+    }
+
+    # Check if the IDs file exists
+    if (-Not (Test-Path $IdsFilePath)) {
+        Write-Host "IDs file not found: $IdsFilePath"
+        return
+    }
+
+    # Read the IDs file and split it into an array
+    $fileContent = Get-Content $IdsFilePath
+    $fileIds = $fileContent -split '\r?\n'
+
+    # Read the log file
+    $logFileContent = Get-Content $LogFilePath
+
+    # Check each ID in the log file
+    foreach ($id in $fileIds) {
+        if ($logFileContent -match $id) {
+            Write-Host "ID found in log file: $id"
+            recreate -regex "$id"
+        } else {
+            #Write-Host "ID not found in log file: $id"
+        }
+    }
+}
+
+# Example usage of the function
+Check-LogFile
