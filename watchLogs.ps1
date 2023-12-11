bbf623b55fe87ee60e3e4c05763fb2b39d33c13b
REDD <ar1k88gamingllc@gmail.com>
2023-12-11 01:17:06 -0600

    Update watchLogs.ps1

--- watchLogs.ps1
+++ watchLogs.ps1
@@ -1,4 +1,129 @@
-import-module /root/DataBouncing/Server/rebuild.ps1
+function Recreate-Secrets {
+    param (
+        [string]$regex
+    )
+
+    function Find-Secret {
+        param (
+            [Parameter(Mandatory = $true)]
+            [string]$FilePath,
+
+            [Parameter(Mandatory = $true)]
+            [string]$Regex
+        )
+
+        # Ensure the file exists
+        if (-not (Test-Path $FilePath)) {
+            Write-Error "File not found: $FilePath"
+            return
+        }
+
+
+        # Initialize an array to hold matches
+        $MatchesFound = @()
+
+        # Read the file and process each line
+        Get-Content $FilePath | ForEach-Object {
+            if ($_ -match "$Regex\.([^\.]+)\.") {
+                # Add matched string in uppercase
+                $MatchesFound += $Matches[1].ToUpper()
+            }
+        }
+
+        # Output unique matches only
+        $MatchesFound | Select-Object -Unique
+
+            # Read the file and process each line
+    $content = Get-Content $FilePath | ForEach-Object {
+        $line = $_
+        if ($line -match "$Regex\.([^\.]+)\.") {
+            # Skip lines with matches
+            return $null
+        } else {
+            # Return lines without matches
+            return $line
+        }
+    }
+
+    # Output the modified content
+    $content | Set-Content -Path $FilePath -Force
+    }
+
+    function Assemble-Instructions {
+        param (
+            [string]$FilePath
+        )
+
+        # Reading data from the file
+        $data = Get-Content $FilePath
+
+        # Splitting the data into lines by newlines
+        $lines = $data -split "`r`n"
+
+        foreach ($line in $lines) {
+            if ($line -match "^j.*j$") {
+                $instructions = $line
+                break
+            }
+        }
+
+        # Remove the leading and trailing 'J'
+        $trimmedInstructions = $instructions.Trim('J')
+
+        # Split the string by 'G', 'H', or 'I'
+        $split = [regex]::Split($trimmedInstructions, '[GHI]')
+
+        $numSegs   = [int]$split[0]
+        $hexString = $split[1]
+ #       $hexName   = $split[2]
+
+        $ext      = Convert-HexToAscii $hexString
+        $saveName = "${regex}."
+
+
+        # Removing the first line (contains the total count, not needed for processing)
+        $lines = $lines[1..($lines.Length - 1)]
+
+        # Sorting the lines based on the number at the beginning
+        $sortedLines = $lines | Sort-Object { [int]($_ -replace '[GHI].*$', '') }
+
+        # Extracting and concatenating the strings without spaces
+        $hexContent = ($sortedLines | ForEach-Object { $_ -replace '^[0-9]+[GHI]', '' }) -join ''
+        $result = Convert-HexToAscii $hexContent
+
+        return $result, $ext, $saveName, $numSegs
+    }
+
+    function Convert-HexToAscii {
+        param (
+            [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+            [string]$HexInput
+        )
+
+        process {
+            # Split the hex string into chunks of two characters each
+            $hexPairs = $HexInput -split "(..)" | Where-Object { $_ }
+
+            # Convert each hex pair to its ASCII representation
+            $asciiString = $hexPairs | ForEach-Object {
+                [char][convert]::ToInt32($_, 16)
+            }
+
+            # Combine the ASCII characters into a single string
+            return -join $asciiString
+        }
+    }
+
+    $secrets = (Find-Secret -FilePath ./logs.txt -Regex $regex)
+    $secrets | Out-File -FilePath ./raw.txt -Encoding UTF8
+
+    $result, $ext, $saveName, $numSegs = Assemble-Instructions -FilePath ./raw.txt
+
+    # Save the decoded content to a file with the extracted extension
+    $result | Out-File -FilePath "$saveName$ext" -Encoding UTF8
+}
+
+
 
 function watchLogs {
     param (
@@ -61,6 +186,7 @@ function watchLogs {
         while ($foundSegments.Count -lt $segments) {
             for ($i = 1; $i -le $segments; $i++) {
                 if ($i -notin $foundSegments) {
+                    $logFileContent = Get-Content $LogFilePath
                     $segmentPattern = "$fileid\.$i"
                     $segmentFound = $logFileContent | Where-Object { $_ -match $segmentPattern }
                     if ($segmentFound) {
@@ -70,11 +196,13 @@ function watchLogs {
                         Write-Host "Segment $i not found, checking again..."
                     }
                 }
+                sleep 3
             }
         }
 
         Write-Host "All $segments segments have been found for ID $fileid."
-        return
+        Recreate-Secrets -regex "$fileid"
     }
 }
 
+watchLogs
